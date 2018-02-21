5b6c63338ab3ec14b75d1c135bed24b302ae623e
qutus <qrouault@gmail.com>
2018-02-21 10:25:32 +0100

    Integrate data model to App.js, just to enable possibility to create a pull request on github

--- src/App.js
+++ src/App.js
@@ -2,7 +2,44 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+const BOARD_SIZE = 10;
+const PLAYER_QUANTITY = 2;
+const MAX_SHIP_PER_PLAYER = 10;
+const SHIP_TOUCHED = 1;
+const SHIP_MISS = 1;
+
 class App extends Component {
+
+    constructor() {
+        state = {
+            playerOneData = [{
+                isAllShipOnBoard: false,
+                shipPosition: [],
+                playerTwoReaminingShip : 0,
+                shipTouchedPosition: [],
+                shipMissedPosition: []
+            }],
+            playerTwoData = [{
+                isAllShipOnBoard: false,
+                shipPosition: [],
+                playerTwoReaminingShip : 0,
+                shipTouchedPosition: [],
+                shipMissedPosition: []
+            }],
+        };
+
+        this.playerOneData = [{
+            remaingShip: MAX_SHIP_PER_PLAYER,
+            playerTwoShipPosition: [],
+            bombSend: 0
+        }];
+        this.playerTwoData = [{
+            remaingShip: MAX_SHIP_PER_PLAYER,
+            playerTwoShipPosition: [],
+            bombSend: 0
+        }];
+    }
+
   render() {
     return (
       <div className="App">
