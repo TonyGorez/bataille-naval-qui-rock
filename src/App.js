3554f868330e1c7247acb3b61e271afeff45c608
Tony Gorez <gorez.tony@gmail.com>
2018-03-01 21:22:27 +0100

    none

--- src/App.js
+++ src/App.js
@@ -2,52 +2,39 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-<<<<<<< HEAD
-import GameView from './components/GameView/GameView';
-import InitView from './components/InitView/InitView';  
-=======
 const BOARD_SIZE = 10;
 const PLAYER_QUANTITY = 2;
 const MAX_SHIP_PER_PLAYER = 10;
-const SHIP_TOUCHED = 1;
-const SHIP_MISS = 1;
->>>>>>> 294bf0ff8f1e7f28a1a952893497ec8acfa3f1e6
+const SHIP_TOUCHED = "X";
+const SHIP_MISS = "M";
+const WATER = 0;
+const BOAT = 1; 
 
 class App extends Component {
 
     constructor(props) {
         super(props); 
+
         this.state = {
             playerOneData : {
                 isAllShipOnBoard: false,
-                shipPosition: [],
-                playerTwoReaminingShip : 0,
-                shipTouchedPosition: [],
-                shipMissedPosition: []
+                reaminingShip : 0,
+                board: []
             },
             playerTwoData : {
                 isAllShipOnBoard: false,
-                shipPosition: [],
-                playerTwoReaminingShip : 0,
-                shipTouchedPosition: [],
-                shipMissedPosition: []
+                reaminingShip : 0,
+                board: []
             },
-        };
-
-        this.playerOneData = {
-            bombSend: 0
-        };
-        this.playerTwoData = {
-            bombSend: 0
         };
     }
 
-  render() {
-    return (
-      <div className="App">
-        <div>YOOOOO</div>
-      </div>
-    );
+    render() {
+        return (
+            <div className="App">
+                <div>YOOOOO</div>
+            </div>
+        );
   }
 }
 
